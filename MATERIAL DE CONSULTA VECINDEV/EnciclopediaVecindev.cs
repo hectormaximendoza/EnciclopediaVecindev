569b9f07b1f9ccc0102d283b80e795e6b8b5f7f3
hectormaximendoza <hectormaximendoza@gmail.com>
2022-08-02 17:49:52 -0700

    CONCEPTOS AGREGADOS OPT 0 Y 1

---
+++ MATERIAL DE CONSULTA VECINDEV/EnciclopediaVecindev.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MATERIAL_DE_CONSULTA_VECINDEV
+{
+    public class EnciclopediaVecindev
+    {
+        public string ConsultarBuenasPracticas()
+        {
+            string mensaje;
+
+            mensaje = "►Las buenas prácticas de programación son un conjunto formal o informal de reglas, pudiendo ser opcionales u obligatorias, que se adoptan con el fin general de mejorar la calidad del software.\n\nPermiten\n\n ► Facilitar el proceso de desarrollo para el programador.\n\n ► Aumentar o mejorar la legibilidad y mantenibilidad del código fuente, lo que ayuda a otros desarrolladoreso a versiones futuras del autor inicial-a comprender el software.\n\n ► Evitar cierta clase de errores comunes, por ejemplo equivocarse por 1 en los límites de una iteración\n\n ► Sentencia, una debajo de otra por comodidad.";
+            
+            return mensaje;
+        }
+        public string ConsultarElementosBasicos()
+        {
+            string mensaje;
+           
+                
+            mensaje = "TIPOS DE VARIABLES\n\nC# proporciona los siguientes tipos de valor integrados, también conocidos como tipos simples: \n\nTipos numéricos integrales \n\nTipos numéricos de punto flotante \n\nbool, que representa un valor booleano \n\nchar, que representa un carácter Unicode UTF - 16\n\n VARIABLES, espacio  memoria, almacena un valor de tipo particular. Las palabras reservadas no se usan más que para llamar a una tarea del compilador (bool, int, while, double, etc). Variables locales. estan escritas dentro de los miembros. Tipos de datos. De valor. \n\nVARIABLES DE tipos simples. enteros con signos: sbyte, short, int, long.. \n\nEnteros sin signos (NUMEROS ADELANTE DEL CERO, ENTEROS) byte, ushort, uint, ulong. \n\nCaracteres unicode: char. \n\npunto flotante binario: float, double. \n\npunto flotante decimal: decimal. \n\nBooleano: bool. \n\nVARIABLES de tipos de enumeracion tipos definidos por el usuario: enum E {. . .} \n\nDe referencia.\n\n\n\nLa diferencia principal entre ConsoleWrite y consoleWriteLine es que el primero refleja el mensaje en la misma linea de codigo,  en cambio WriteLine escribe los datos especificados, seguidos del terminador de línea actual (salto de linea)" + "\n\n\nDECLARACION DE VARIABLE [VARIABLE] [TIPO(nombre de identificador)];\n\n\nDARLE UN VALOR A LA VARIABLE (INICIALIZAR UNA VARIABLE) \n[TIPO] = 500; \n\nEL OPERADOR DE ASIGNACION ES = ASIGNA EL VALOR DE SU DERECHA A LOS QUE TENGA DE SU LADO IZQUIERDO\n\nDECLARAR E INICIALIZAR UNA VARIABLE \n\n[VARIABLE] [TIPO] = 5000;";
+
+          
+            return mensaje;
+        }
+    }
+}
---
+++ MATERIAL DE CONSULTA VECINDEV/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MATERIAL_DE_CONSULTA_VECINDEV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //VARIABLES
+            int opcion;
+            //PRESENTACION
+            Console.WriteLine("MATERIAL DE CONSULTA DE LA VECINDEV - C#");
+            Console.WriteLine("----------------------------------------");
+            Console.ReadKey();
+            Console.Clear();
+            Console.Beep();
+
+            //INSTANCIAMOS LA CLASE
+            EnciclopediaVecindev material1 = new EnciclopediaVecindev();
+
+            //INICIO MENU
+            do
+            {
+                Console.Write("---------------------------------------");
+                Console.WriteLine("MATERIAL DE CONSULTA C#");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("0. BUENAS PRACTICAS DE PROGRAMACION");
+                Console.WriteLine("1. ELEMENTOS BASICOS DE UN PROGRAMA DE COMPUTADORA");
+                Console.WriteLine("2. LA TOMA DE DECISIONES");
+                Console.WriteLine("3. LOS CICLOS");
+                Console.WriteLine("4. LOS METODOS");
+                Console.WriteLine("5. LAS MATRICES");
+                Console.WriteLine("6. LAS CLASES");
+                Console.WriteLine("7. LAS COLECCIONES GENERICAS");
+                Console.WriteLine("8. LAS CADENAS");
+                Console.WriteLine("9. ESTRUCTURAS Y ENUMERACIONES");
+                Console.WriteLine("10. FLUJOS Y ARCHIVOS");
+                Console.WriteLine("11. DEPURACION");
+                Console.WriteLine("12. SALIR");
+
+                Console.Write("\nElija una opción: ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                
+                switch (opcion)
+                {
+                    
+                    case 0:
+                        Console.WriteLine(material1.ConsultarBuenasPracticas());
+                        break;
+                    case 1:
+                        Console.WriteLine(material1.ConsultarElementosBasicos());
+                        break;
+
+                        
+
+                    //    break;
+                    //case 2:
+                    //    Console.Write("Ingrese el monto a retirar: $");
+                    //    = Convert.ToDouble(Console.ReadLine());
+                    //    break;
+                    //case 3:
+                    //    break;
+                    //case 4:
+                    //    Console.WriteLine(.ToString());
+                    //    break;
+                }
+
+
+            } while (opcion >= 0 && opcion <= 4);
+
+
+        }
+    }
+}
